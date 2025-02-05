2af72e76ba9db528019dd8a83588cd1db6441b7c
Stephen Weeks <stweeks@ruby.cecs.pdx.edu>
2025-02-04 19:45:43 -0800

    Adding registers and build system

---
+++ include/regfile.hpp
@@ -0,0 +1,29 @@
+#ifndef _REGFILE_HPP_
+#define _REGFILE_HPP_
+#include <cstdint>
+#include <array>
+
+constexpr uint8_t PC = 32;
+constexpr uint8_t zero = 0;
+constexpr uint8_t ra = 1;
+constexpr uint8_t sp = 2;
+constexpr uint8_t gp = 3;
+constexpr uint8_t tp = 4;
+constexpr uint8_t fp = 8;
+constexpr uint8_t REGWIDTH = 32;
+
+class RegFile
+{
+	private:
+		std::array<uint32_t, 33> registers;
+	
+	public:
+		RegFile(int PCLoc, int SPLoc);
+		uint32_t read(uint8_t val);
+		void write(uint8_t idx, uint32_t val);
+		void updatePC(uint32_t val);
+		uint32_t readPC(void);
+		void print(void);
+};
+
+#endif // _REGFILE_HPP_
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "regfile.hpp"
+#include <cstdio>
+
+int main(int argc, char *argv[]) {
+	// read and interprete CLI
+	
+
+	// program exit point
+	return 0;
+}
