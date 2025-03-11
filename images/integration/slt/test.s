76d28e8195bb7782c5d61e79532e6b8ca9127194
Riley Cox <rileyc39@gmail.com>
2025-03-11 13:19:22 -0700

    SLT integration test

---
+++ images/integration/slt/test.s
@@ -0,0 +1,26 @@
+	.section .text
+	.align 2
+	.global _start
+	
+	.equ spn, 50
+	.equ bpn, 100
+	.equ snn, -300
+	.equ bnn, -50
+
+
+_start:
+	mv	ra, zero
+	li	t0, spn
+	li	t1, bpn
+	li	t2, snn
+	li	t3, bnn
+ 	slt	t4, t0, t1
+	slt	t5, t2, t3
+	slt	t6, t3, t0
+	slt	s1, zero, t0
+	slt	s2, t1, t0
+	slt	s3, t3, t2
+	slt	s4, t1, t1
+	slt 	s5, zero, zero
+	jr	ra
+		
