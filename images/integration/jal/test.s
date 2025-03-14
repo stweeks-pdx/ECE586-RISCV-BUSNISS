53085eebb52815fb73aa21dfc025c7d8f283c661
Nathaniel Fraly <nfraly85@gmail.com>
2025-03-14 10:50:35 -0700

    jal integration testing

--- images/integration/jal/test.s
+++ images/integration/jal/test.s
@@ -2,13 +2,20 @@
 		.align 2
 		.global _start
 
-.l2:
-	li s0, 0x7ee
-	jal t0, 0x008
-	jr ra
 _start:
-	
+	li t0, 0x5
 	jal t0, .l2
+	li s2, 0xfeef
+	jal t1, 0x2
 	jr ra
+
+.l3:
+	li s0, 0xdeadbeef
+	jr t1
+
+.l2:
+	li s1, 0xbeefdead
+	jal t1, .l3
+	jr t0
 
 
