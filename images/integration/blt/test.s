2a248fd2de42ef6cdd5aaf13fb04a3b450e956de
Nathaniel Fraly <nfraly85@gmail.com>
2025-03-16 15:50:56 -0700

    BLT integration testing

---
+++ images/integration/blt/test.s
@@ -0,0 +1,48 @@
+		.section .text
+		.align 2
+		.global _start
+
+_start:
+	li t0, 3
+	li t1, 4
+	li t2, -3
+	li t3, -4
+	li t4, 3
+	li s0, 0
+
+	blt t0, t1, .L1
+	blt t1, t0, .L2
+	blt t0, t4, .L3
+	blt t2, t0, .L4
+	blt t0, t2, .L5
+	jr ra
+
+.L1:
+	addi s0, s0, 1
+	blt t1, t0, .L2
+	blt t0, t4, .L3
+	blt t2, t0, .L4
+	blt t0, t2, .L5
+	jr ra
+
+.L2:
+	addi s0, s0, -1
+	blt t0, t4, .L3
+	blt t2, t0, .L4
+	blt t0, t2, .L5
+	jr ra
+
+.L3:
+	addi s0, s0, -1
+	blt t2, t0, .L4
+	blt t0, t2, .L5
+	jr ra
+
+.L4:
+	addi s0, s0, 1
+	blt t0, t2, .L5
+	jr ra
+
+.L5:
+	addi s0, s0, -1
+	jr ra
