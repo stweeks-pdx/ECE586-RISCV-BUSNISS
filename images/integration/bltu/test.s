2c3ee9b5d1378e20ac7657235adfa8adaffc57e5
Nathaniel Fraly <nfraly85@gmail.com>
2025-03-16 19:58:58 -0700

    Adding misaligned memory reference tests

--- images/integration/bltu/test.s
+++ images/integration/bltu/test.s
@@ -1,48 +1,57 @@
-		.section .text
-		.align 2
-		.global _start
+                .section .text
+                .align 2
+                .global _start
 
 _start:
-	li t0, 3
-	li t1, 4
-	li t2, -3
-	li t3, -4
-	li t4, 3
-	li s0, 0
+        li t0, 3
+        li t1, 4
+        li t2, -3
+        li t3, -4
+        li t4, 3
+        li s0, 0
+        j .begin
 
-	bltu t0, t1, .L1
-	bltu t1, t0, .L2
-	bltu t0, t4, .L3
-	bltu t2, t0, .L4
-	bltu t0, t2, .L5
-	jr ra
+.misalign:
+        bltu t1, t0, mem_loc_2
+        mem_loc_1: .half 0x0
+        mem_loc_2: .half 0x0
+        jr ra
+
+.begin:
+        bltu t0, t1, .L1
+        bltu t1, t0, .L2
+        bltu t0, t4, .L3
+        bltu t2, t0, .L4
+        bltu t0, t2, .L5
+        j .misalign
 
 .L1:
-	addi s0, s0, 1
-	bltu t1, t0, .L2
-	bltu t0, t4, .L3
-	bltu t2, t0, .L4
-	bltu t0, t2, .L5
-	jr ra
+        addi s0, s0, 1
+        bltu t1, t0, .L2
+        bltu t0, t4, .L3
+        bltu t2, t0, .L4
+        bltu t0, t2, .L5
+        j .misalign
 
 .L2:
-	addi s0, s0, -1
-	bltu t0, t4, .L3
-	bltu t2, t0, .L4
-	bltu t0, t2, .L5
-	jr ra
+        addi s0, s0, -1
+        bltu t0, t4, .L3
+        bltu t2, t0, .L4
+        bltu t0, t2, .L5
+        j .misalign
 
 .L3:
-	addi s0, s0, -1
-	bltu t2, t0, .L4
-	bltu t0, t2, .L5
-	jr ra
+        addi s0, s0, -1
+        bltu t2, t0, .L4
+        bltu t0, t2, .L5
+        j .misalign
 
 .L4:
-	addi s0, s0, -1
-	bltu t0, t2, .L5
-	jr ra
+        addi s0, s0, -1
+        bltu t0, t2, .L5
+        j .misalign
 
 .L5:
-	addi s0, s0, 1
-	jr ra
+        addi s0, s0, 1
+        j .misalign
+
